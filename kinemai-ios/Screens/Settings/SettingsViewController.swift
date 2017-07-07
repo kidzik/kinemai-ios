7ad389c4925db857834ccf6944c8bea0e8aaa6ef
Lukasz Kidzinski <kidzik@macbook-pro-de-magdalena.local>
2017-07-06 23:02:08 -0700

    Text in onboarding, some more info in Settings.

--- kinemai-ios/Screens/Settings/SettingsViewController.swift
+++ kinemai-ios/Screens/Settings/SettingsViewController.swift
@@ -11,9 +11,25 @@ import UIKit
 class SettingsViewController: UIViewController
 {
     @IBOutlet weak var btnInfo: UIBarButtonItem!
+    @IBOutlet weak var btnSurvey: UIButton!
+    @IBOutlet weak var btnEmail: UIButton!
     
     @IBAction func btnInfoPressed()
     {
         UIApplication.shared.showOnboarding()
+    }
+    @IBAction func btnSurveyPressed(_ sender: UIButton) {
+        let url = URL(string: "https://goo.gl/forms/ZQqf6nplgBoOBUpU2")!
+        if #available(iOS 10.0, *) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        } else {
+            UIApplication.shared.openURL(url)
+        }
+    }
+    @IBAction func btnEmailPressed(_ sender: UIButton) {
+        let email = "hello@kinemai.com"
+        if let url = URL(string: "mailto:\(email)") {
+            UIApplication.shared.open(url)
+        }
     }
 }
--- kinemai-ios/UIApplication+ShowOnboarding.swift
+++ kinemai-ios/UIApplication+ShowOnboarding.swift
@@ -11,7 +11,23 @@ import Onboard
 
 extension UIApplication
 {
-    static let KEY_didUserSeeOnboarding = "KEY_USER_SAW_ONBOARDING"
+    static func randomString(length: Int) -> String {
+        
+        let letters : NSString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        let len = UInt32(letters.length)
+        
+        var randomString = ""
+        
+        for _ in 0 ..< length {
+            let rand = arc4random_uniform(len)
+            var nextChar = letters.character(at: Int(rand))
+            randomString += NSString(characters: &nextChar, length: 1) as String
+        }
+        
+        return randomString
+    }
+    
+    static let KEY_didUserSeeOnboarding = "KEY_USER_SAW_ONBOARDING" // + randomString(length: 10)
     
     var didUserSeeOnboarding: Bool {
         let ud = UserDefaults.standard
@@ -30,29 +46,50 @@ extension UIApplication
         
         
         let pageOne = OnboardingContentViewController(
-            title: "Page One"
-            , body: nil
+            title: "Welcome to Kinemai Golf"
+            , body: "Kinemai will help you swing like a PRO.\n\n" +
+            "Just record your swing from one of the two angles.\n\n" +
+            "Swipe to the next screens for details.\n"
             , image: nil
             , buttonText: nil
             , action: nil
         )
         
         let pageTwo = OnboardingContentViewController(
-            title: "Page Two"
-            , body: nil
-            , image: nil
+            title: "Face on"
+            , body: "Put the phone in front.\n\n" +
+            "Kinemai will analyze your pose and the strike."
+            , image: UIImage(named: "Onboarding/faceon")
             , buttonText: nil
             , action: nil
         )
         
         let pageThree = OnboardingContentViewController(
-            title: "Page Three"
-            , body: nil
-            , image: nil
+            title: "Down the line"
+            , body: "Put the phone on side.\n\n" +
+            "Kinemai will analyze your alignment and your swing path."
+            , image: UIImage(named: "Onboarding/downtheline")
             , buttonText: nil
             , action: nil
         )
         
+        pageOne.topPadding = 50
+        pageOne.iconHeight = 0
+        pageOne.underIconPadding = 10
+        
+        pageTwo.topPadding = 35
+        pageTwo.iconHeight = 265
+        pageTwo.iconWidth = 200
+        pageTwo.underIconPadding = 10
+        pageTwo.underTitlePadding = 5
+        
+        pageThree.topPadding = 35
+        pageThree.iconHeight = 265
+        pageThree.iconWidth = 180
+        pageThree.underIconPadding = 10
+        pageThree.underTitlePadding = 5
+
+
         let backgroundImage = UIImage(named: "Onboarding/background")!
         let onboarding = OnboardingViewController(
             backgroundImage: backgroundImage
@@ -64,6 +101,7 @@ extension UIApplication
         onboarding.skipHandler = {
             onboarding.presentingViewController?.dismiss(animated: true, completion: nil)
         }
+        onboarding.skipButton.setTitle("Let's go", for: .normal)
         
         tabVC.present(onboarding, animated: true, completion: nil)
     }
